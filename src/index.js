250750335be5c49e26b2cc415b31f93753f94422
aizada.turarova <aizada.turarova>
2022-05-12 17:40:34 +0600

    Add navigate option

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { sum } from "./modules/sum";
+// import { sum } from "./modules/sum";
 
 const root = document.querySelector("#root");
-root.textContent = sum(6, -1).toString();
--- src/modules/sum.js
+++
@@ -1,7 +0,0 @@
-export function sum(...args) {
-    if (args.length === 0) {
-        throw Error("Должно быть больше 1 аргумента");
-    }
-
-    return args.reduce((result, current) => result + current, 0);
-}
